ebb772757c6b37bc29d845f75663c3eae88a77b9
Gautier <gcliquet@gmail.com>
2020-02-09 15:00:55 +0100

    correction of a syntax error result instead of results

--- app/src/main/java/com/cliquet/gautier/go4lunch/Models/GoogleMapsApi/Pojo/DetailsPojo.java
+++ app/src/main/java/com/cliquet/gautier/go4lunch/Models/GoogleMapsApi/Pojo/DetailsPojo.java
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class DetailsPojo {
-    @SerializedName("results")
+    @SerializedName("result")
     @Expose
     private Results results;
 
