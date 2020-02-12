5ae1fdef7c39631521881965faa8b86da20a270d
Ocelot <gcliquet@gmail.com>
2020-02-12 16:57:58 +0100

    WebsiteActivity is working, websites are watchable

--- app/src/main/java/com/cliquet/gautier/go4lunch/Controllers/Activities/WebsiteActivity.java
+++ app/src/main/java/com/cliquet/gautier/go4lunch/Controllers/Activities/WebsiteActivity.java
@@ -3,6 +3,7 @@ package com.cliquet.gautier.go4lunch.Controllers.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.cliquet.gautier.go4lunch.R;
 
@@ -12,5 +13,12 @@ public class WebsiteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_website);
+
+        String url = getIntent().getStringExtra("url");
+
+        WebView webview;
+        webview = findViewById(R.id.activity_website_webview);
+
+        webview.loadUrl(url);
     }
 }
