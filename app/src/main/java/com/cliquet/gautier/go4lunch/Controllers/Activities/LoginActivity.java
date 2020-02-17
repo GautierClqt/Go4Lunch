0c21c7214071aef0c84303e72460c0ae88fb586e
Ocelot <gcliquet@gmail.com>
2020-02-17 19:19:52 +0100

    sign-in with a google account works

--- app/src/main/java/com/cliquet/gautier/go4lunch/Controllers/Activities/LoginActivity.java
+++ app/src/main/java/com/cliquet/gautier/go4lunch/Controllers/Activities/LoginActivity.java
@@ -3,12 +3,40 @@ package com.cliquet.gautier.go4lunch.Controllers.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.cliquet.gautier.go4lunch.R;
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    Button logInGoogleButton;
+    Button logInFacebookButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        logInGoogleButton = findViewById(R.id.activity_login_google_button);
+
+        logInGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                .setLogo(R.drawable.ic_logo_go4lunch)
+                                .build(),
+                        RC_SIGN_IN);
+
+            }
+        });
     }
 }
