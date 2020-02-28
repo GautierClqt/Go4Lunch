b6ecfc7fe64327c803243d8300b1a6f66501f01e
Ocelot <gcliquet@gmail.com>
2020-02-28 16:37:56 +0100

    cleaning file to properly redo it

--- app/src/test/java/com/cliquet/gautier/go4lunch/HoursUnitTest.java
+++ app/src/test/java/com/cliquet/gautier/go4lunch/HoursUnitTest.java
@@ -22,31 +22,9 @@ import static org.junit.Assert.*;
  */
 
 public class HoursUnitTest {
-//    public static final String ASSET_BASE_PATH = "../app/src/main/assets";
-//
-//    public String readJsonFile(String filename) throws IOException {
-//
-//        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(ASSET_BASE_PATH + filename)));
-//        StringBuilder sb = new StringBuilder();
-//        String line = br.readLine();
-//        while(line != null) {
-//            sb.append(line);
-//            line = br.readLine();
-//        }
-//
-//        return sb.toString();
-//    }
-//
-//    @Test
-//    public void testCorrectHoursAreDisplayedInListFragment() {
-//
-//        Gson gson = new GsonBuilder().create();
-//        Event mockHours = gson.fromJson(readJsonFile("mock_hours.json"), Event.EventList.class);
-//        eventDatesLiest = gson.fromJson(readJsonFile("mock_hours.json"), EventDates.EventDatesList.class);
-//
-//        classLoader.getResource("mock_hours.json");
-//
-//        List
-//        assertEquals(4, 2 + 2);
-//    }
+    @Test
+    public void testCorrectHoursAreDisplayedInListFragment() {
+
+        assertEquals(4, 2 + 2);
+    }
 }
