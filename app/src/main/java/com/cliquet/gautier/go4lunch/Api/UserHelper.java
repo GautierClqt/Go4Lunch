8a58a1b63ed6a876cd1398f7ad48367d4ad1fc4f
Ocelot <gcliquet@gmail.com>
2020-03-06 15:19:54 +0100

    create and add first methods of UserHelper.java

---
+++ app/src/main/java/com/cliquet/gautier/go4lunch/Api/UserHelper.java
@@ -0,0 +1,32 @@
+package com.cliquet.gautier.go4lunch.Api;
+
+import com.cliquet.gautier.go4lunch.Models.User;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class UserHelper {
+
+    private static final String COLLECTION_NAME = "users";
+
+    public static CollectionReference getUsersCollection() {
+        return FirebaseFirestore.getInstance().collection(COLLECTION_NAME);
+    }
+
+    //CREATE
+    public static Task<Void> createUser(String userId, String userFirstName, String userLastName, String userEmail, String userUrlPicture) {
+        User userToCreate = new User(userId, userFirstName, userLastName, userEmail, userUrlPicture);
+        return UserHelper.getUsersCollection().document(userId).set(userToCreate);
+    }
+
+    //GET
+    public static Task<DocumentSnapshot> getUser(String userId) {
+        return UserHelper.getUsersCollection().document(userId).get();
+    }
+
+    //DELETE
+    public static Task<Void> deleteUser(String userId) {
+        return UserHelper.getUsersCollection().document(userId).delete();
+    }
+}
