17c93d5fd08eb9f99ad52f41c05a4bd5395c66c1
Ocelot <gcliquet@gmail.com>
2020-03-23 14:22:36 +0100

    LoginActivity refactoring to properly use of AuthUI.getInstance() method

--- app/src/main/java/com/cliquet/gautier/go4lunch/Controllers/Activities/LoginActivity.java
+++ app/src/main/java/com/cliquet/gautier/go4lunch/Controllers/Activities/LoginActivity.java
@@ -3,23 +3,17 @@ package com.cliquet.gautier.go4lunch.Controllers.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import com.cliquet.gautier.go4lunch.Api.UserHelper;
 import com.cliquet.gautier.go4lunch.R;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final int GOOGLE_SIGN_IN = 123;
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +35,14 @@ public class LoginActivity extends AppCompatActivity {
                                         new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
                         .setIsSmartLockEnabled(false, true)
                         .build(),
-                GOOGLE_SIGN_IN);
+                RC_SIGN_IN);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == GOOGLE_SIGN_IN) {
+        if(requestCode == RC_SIGN_IN) {
             IdpResponse response = IdpResponse.fromResultIntent(data);
 
             if(FirebaseAuth.getInstance().getCurrentUser().getUid() != null) {
