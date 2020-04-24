bb52e072ed4da9960603337c8fef6968836838d5
Ocelot <gcliquet@gmail.com>
2020-04-24 15:52:28 +0200

    app properly checks if authenticating user already exists in Firestore or not

--- app/src/main/java/com/cliquet/gautier/go4lunch/Controllers/Activities/LoginActivity.java
+++ app/src/main/java/com/cliquet/gautier/go4lunch/Controllers/Activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.cliquet.gautier.go4lunch.Controllers.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,8 +8,11 @@ import android.os.Bundle;
 import com.cliquet.gautier.go4lunch.Api.UserHelper;
 import com.cliquet.gautier.go4lunch.R;
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Arrays;
@@ -28,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void checkForExistingAuthentication() {
         if(FirebaseAuth.getInstance().getCurrentUser() != null) {
-            launchMainActivity();
+            startMainActivity();
         }
         else{
             startSignInActivity();
@@ -54,32 +58,51 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+
+
         if(requestCode == RC_SIGN_IN) {
             if(resultCode == RESULT_OK) {
-
-                if (FirebaseAuth.getInstance().getCurrentUser().getUid() != null) {
-                    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
-                    String fullName = firebaseUser.getDisplayName();
-                    String[] splitNames = fullName.split(" ");
-
-                    UserHelper.createUser(
-                            firebaseUser.getUid(),
-                            splitNames[0],
-                            splitNames[1],
-                            firebaseUser.getEmail(),
-                            firebaseUser.getPhotoUrl().toString()
-                    );
-                }
-                launchMainActivity();
+                checkForExistingUser(FirebaseAuth.getInstance().getCurrentUser().getUid());
             }
         }
     }
 
-    private void launchMainActivity() {
+    private void startMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void createNewUserInFirestore() {
+        if (FirebaseAuth.getInstance().getCurrentUser().getUid() != null) {
+            FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+            String fullName = firebaseUser.getDisplayName();
+            String[] splitNames = fullName.split(" ");
+
+            UserHelper.createUser(
+                    firebaseUser.getUid(),
+                    splitNames[0],
+                    splitNames[1],
+                    firebaseUser.getEmail(),
+                    firebaseUser.getPhotoUrl().toString()
+            );
+        }
+        startMainActivity();
+    }
+
+    private void checkForExistingUser(String userId) {
+        UserHelper.getUser(userId).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.getResult().exists()) {
+                    startMainActivity();
+                }
+                else {
+                    createNewUserInFirestore();
+                }
+            }
+        });
     }
 
 
