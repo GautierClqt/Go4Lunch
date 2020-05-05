5cdcd5b8dcf649664ef73e99c56b94f982cc523b
Ocelot <gcliquet@gmail.com>
2020-05-05 16:48:41 +0200

    create SettingsActivity, set a basic layout and strings(with translations)

---
+++ app/src/main/java/com/cliquet/gautier/go4lunch/Controllers/Activities/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.cliquet.gautier.go4lunch.Controllers.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cliquet.gautier.go4lunch.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
